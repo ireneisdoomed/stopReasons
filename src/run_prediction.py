492c03439f7af4ad497c245857f1911f76371d86
Irene López <irene.lopezs@protonmail.com>
2022-09-12 10:49:52 +0100

    feat: load classifier from the hub if not found in local

--- src/run_prediction.py
+++ src/run_prediction.py
@@ -1,21 +1,19 @@
-import sparknlp
-from sparknlp.base import *
-from sparknlp.annotator import *
+
+from pathlib import Path
 from pyspark.ml import Pipeline
 from pyspark.sql import SparkSession
 import pyspark.sql.functions as F
+import sparknlp
+from sparknlp.base import *
+from sparknlp.annotator import *
 
 from utils import prepare_model_from_hub
 
 def save_spark_nlp_classifier(classifier, model_name:str) -> None:
     classifier.write().overwrite().save(f"models/{model_name}_spark_nlp")
 
-def main(spark_instance, model_name):
-    
-    # Save and format model from HuggingFace Hub to be ingested by Spark NLP
-    # prepare_model_from_hub(model_name)
-    
-    classifier = (
+def load_spark_nlp_classifier(model_name:str, spark_instance):
+    return (
         BertForTokenClassification
         .loadSavedModel(
             f"models/{model_name}/saved_model/1",
@@ -26,7 +24,27 @@ def main(spark_instance, model_name):
         .setCaseSensitive(False)
         .setMaxSentenceLength(512)
     )
-    save_spark_nlp_classifier(classifier, model_name)
+
+def prepare_spark_nlp_classifier(model_name:str, model_dir: str, spark_instance):
+
+    classifier = load_spark_nlp_classifier(model_name, spark_instance)
+    
+    # export the model if it has not been exported yet
+    spark_model_path = f"{model_dir}/{model_name}_spark_nlp"
+    if not Path(spark_model_path).is_dir():
+        save_spark_nlp_classifier(classifier, model_name)
+    
+    return classifier
+
+def main(spark_instance, model_name, model_dir):
+    
+    # Save and format model from HuggingFace Hub to be ingested by Spark NLP
+    prepare_model_from_hub(model_name, model_dir)
+    spark_model = prepare_spark_nlp_classifier(model_name, model_dir, spark_instance)
+
+
+
+    
 
     # df = spark_instance.read.json('data/cttv008-25-08-2022.json.gz')
 
@@ -36,6 +54,6 @@ if __name__ == "__main__":
 
     spark = sparknlp.start()
 
-    main(spark_instance=spark, model_name='opentargets/stop_reasons_classificator')
+    main(spark_instance=spark, model_name='opentargets/stop_reasons_classificator', model_dir='models')
 
 
--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,16 @@
 import shutil
 
+from pathlib import Path
 from transformers import AutoTokenizer, TFAutoModelForSequenceClassification
 
 def export_labels_to_model(model_name: str, model) -> None:
+    """
+    Reads from a model configuration to export the labels of the class target to a file in the model's assets folder.
+    
+    Args:
+      model_name (str): The name of the model. This is used to create a directory for the model.
+      model: The model to export.
+    """
     labels = model.config.label2id
     labels = sorted(labels, key=labels.get)
 
@@ -13,8 +21,8 @@ def export_labels_to_model(model_name: str, model) -> None:
 
 def save_model_from_hub(model_name: str) -> None:
     """
-    > We load the model and tokenizer from the hub, save them to the `models` directory, and print a
-    message to the console
+    > We load the model and tokenizer from the HuggingFace hub, save them to the `models` directory, and then export
+    the labels of the model to the directory that contains all the assets.
     
     Args:
       model_name (str): The name of the model you want to save.
@@ -33,6 +41,9 @@ def copy_tokenizer_vocab_to_model(model_name):
     """
     > We copy the tokenizer's vocabulary to the model's directory, so that we can use the model for
     predictions.
+
+    Args:
+        model_name (str): The name of the model you want to use.
     """
 
     tokenizer_vocab_path = f'models/{model_name}_tokenizer/vocab.txt'
@@ -41,14 +52,18 @@ def copy_tokenizer_vocab_to_model(model_name):
     shutil.copyfile(tokenizer_vocab_path, f'{model_assets_path}/vocab.txt')
     
 
-def prepare_model_from_hub(model_name: str) -> None:
+def prepare_model_from_hub(model_name: str, model_dir:str) -> None:
     """
-    > We load the model and tokenizer from the hub, save them to the `models` directory, and print a
-    message to the console
+    > If the model directory doesn't exist, download the model from the HuggingFace Hub, and copy the tokenizer
+    vocab to the model directory so that the format can be digested by Spark NLP.
     
     Args:
-      model_name (str): The name of the model you want to save.
+      model_name (str): The name of the model you want to use.
+      model_dir (str): The directory where the model will be saved.
     """
 
-    save_model_from_hub(model_name)
-    copy_tokenizer_vocab_to_model(model_name)
+    model_path = f'{model_dir}/{model_name}'
+
+    if not Path(model_path).is_dir():
+        save_model_from_hub(model_name)
+        copy_tokenizer_vocab_to_model(model_name)
