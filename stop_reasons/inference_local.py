a351d70e99376aa18f12adc50a078176ce5ed2ce
Irene López <irene.lopezs@protonmail.com>
2023-02-05 15:28:40 +0000

    feat: code to test local model

---
+++ stop_reasons/inference_local.py
@@ -0,0 +1,27 @@
+from transformers import AutoTokenizer, TFAutoModelForSequenceClassification
+import tensorflow as tf
+
+
+def main(text):
+    # TO-DO: parametrise local model
+    tokenizer = AutoTokenizer.from_pretrained("./model_3_epochs_classificator_tf", local_files_only=True, from_pt=False)
+    model = TFAutoModelForSequenceClassification.from_pretrained("./model_3_epochs_classificator_tf", local_files_only=True)
+
+    encoded_input = tokenizer(text, return_tensors="tf")
+    logits = model(**encoded_input).logits
+    probs = tf.nn.softmax(logits)
+    print("All probabilities:", probs)
+
+    print("Top 3 classes:")
+    top_3_classes = tf.math.top_k(probs, k=3)
+    for i in range(3):
+        index = int(top_3_classes.indices[0][i])
+        prob = float(top_3_classes.values[0][i])
+        print("... Class:", model.config.id2label[index])
+        print("... Probability:", prob)
+        print("--------------------")
+
+
+if __name__ == '__main__':
+    text = input("Enter text: ")
+    main(text)
